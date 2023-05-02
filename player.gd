1def7aa25ad6c4df0b8f0c31ecda0037cbadf4fb
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-05-02 21:01:23 +0700

    Character flies when player clicks LMB

---
+++ player.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+var fly_speed = -1500
+var flying = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	if Input.is_action_pressed("fly"):
+		if not flying:
+			flying = true
+			set_linear_velocity(Vector2(0, 0))
+		apply_central_force(Vector2(0, fly_speed))
+	if Input.is_action_just_released("fly"):
+		flying = false
