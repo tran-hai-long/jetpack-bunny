57865218dda4d0159f33f75c5915e22bae04798d
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-05-02 21:04:16 +0700

    Only display jetpack flame when Player is flying

--- player.gd
+++ player.gd
@@ -10,7 +10,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if flying:
+		$FlameSprite.show()
+	else:
+		$FlameSprite.hide()
 
 func _physics_process(delta):
 	if Input.is_action_pressed("fly"):
