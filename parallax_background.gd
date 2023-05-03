9204a06fb76bf9886c41b8657099c25bc2e94fc6
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-05-04 05:05:49 +0700

    Create infinite scrolling floor

--- parallax_background.gd
+++ parallax_background.gd
@@ -9,7 +9,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	scroll_offset += Vector2(-1, 0)
-	print(scroll_offset)
 	# 3072 = the point at which background patterns repeat
 	if scroll_offset == Vector2(-3072, 0):
 		scroll_offset = Vector2(0, 0)
