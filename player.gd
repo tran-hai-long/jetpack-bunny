fd9f8a66c665ec237099b4d3fd03b154879043a4
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-05-04 04:20:39 +0700

    Create ceiling

--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 var fly_speed = -1500
 var flying = false
-
+var ceiling_overlapping_bodies
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$BunnySprite.stop()
@@ -23,6 +23,10 @@ func _physics_process(delta):
 		apply_central_force(Vector2(0, fly_speed))
 	if Input.is_action_just_released("fly"):
 		flying = false
+	ceiling_overlapping_bodies = get_tree().current_scene.find_child("CeilingArea").get_overlapping_bodies()
+	for body in ceiling_overlapping_bodies:
+		if body.name == "Player":
+			set_linear_velocity(Vector2(0, 0))
 
 func _on_floor_area_body_entered(body):
 	if(body.name == "Player"):
@@ -32,3 +36,4 @@ func _on_floor_area_body_entered(body):
 func _on_floor_area_body_exited(body):
 	if(body.name == "Player"):
 		$BunnySprite.stop()
+
