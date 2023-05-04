e06db52c712d71b98cf37bf63ee0c4b6a408a0f1
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-05-04 08:55:21 +0700

    Reposition obstacle sprites & collisionshapes

--- main.gd
+++ main.gd
@@ -1,21 +1,25 @@
 extends Node2D
 
-var min_interval = 1
-var max_interval = 2
-var interval = 1.5
+var min_interval = 0.5
+var max_interval = 1
+var interval = 0.75
 var max_player_y
 var min_player_y
 var up_or_down = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$PlaceholderFlyingEnemy.process_mode = Node.PROCESS_MODE_DISABLED
+	$PlaceholderSpikePointsDown.process_mode = Node.PROCESS_MODE_DISABLED
+	$PlaceholderSpikePointsUp.process_mode = Node.PROCESS_MODE_DISABLED
+	$PlaceholderStandingEnemy.process_mode = Node.PROCESS_MODE_DISABLED
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	interval -= delta
 	if interval <= 0:
+		generate_interval()
 		calculate_max_distance()
 		up_or_down = randi_range(0, 1)
 		if up_or_down == 0:
@@ -25,7 +29,6 @@ func _process(delta):
 			# TODO: generate obstacles so that player can go up
 			pass
 		up_or_down = -1
-		generate_interval()
 
 func _physics_process(delta):
 	pass
@@ -35,5 +38,5 @@ func generate_interval():
 	interval = randf_range(min_interval, max_interval)
 	
 func calculate_max_distance():
-	max_player_y = $Player.y + ($Player.fly_speed * interval * interval) / 2
-	min_player_y = $Player.y + (ProjectSettings.get_setting("physics/2d/default_gravity") * interval * interval) / 2
+	max_player_y = $Player.position.y + ($Player.fly_speed * interval * interval) / 2
+	min_player_y = $Player.position.y + (ProjectSettings.get_setting("physics/2d/default_gravity") * interval * interval) / 2
