00afc0c1089c0b18d77cd8b14f50098431710150
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-05-05 06:31:40 +0700

    Display score based of time elapsed

--- main.gd
+++ main.gd
@@ -4,17 +4,16 @@ var screen_size
 var min_interval = 0.5
 var max_interval = 2
 var interval = 0.75
-#var max_player_y
-#var min_player_y
-#var up_or_down = -1
 var obstacle
 var flying_enemy_scene = load("res://obstacles/flying_enemy.tscn")
 var standing_enemy_scene = load("res://obstacles/standing_enemy.tscn")
 var spike_up_scene = load("res://obstacles/spike_points_up.tscn")
 var spike_down_scene = load("res://obstacles/spike_points_down.tscn")
+var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$HUD/ScoreTimer.start()
 	screen_size = get_viewport_rect().size
 
 
@@ -23,40 +22,9 @@ func _process(delta):
 	interval -= delta
 	if interval <= 0:
 		generate_interval()
-		var obstacle_scale = randf_range(0.5, 2)
-		var random_float = randf()
-		if random_float < 0.6:
-			obstacle = flying_enemy_scene.instantiate()
-			obstacle.position.y = randi_range($Ceiling.position.y, $Floor.position.y - obstacle.get_node("FlyingEnemySprite").get_rect().size.y * obstacle_scale)
-		elif random_float < 0.8:
-			obstacle = spike_down_scene.instantiate()
-			obstacle.position.y = $Ceiling.position.y
-		elif random_float < 0.9:
-			obstacle = spike_up_scene.instantiate()
-			obstacle.position.y = $Floor.position.y - obstacle.get_node("SpikePointsUpSprite").get_rect().size.y * obstacle_scale
-		else:
-			obstacle = standing_enemy_scene.instantiate()
-			obstacle.position.y = $Floor.position.y - obstacle.get_node("StandingEnemySprite").get_rect().size.y * obstacle_scale
-		obstacle.position.x = screen_size.x
-		obstacle.scale.x = obstacle_scale
-		obstacle.scale.y = obstacle_scale
-		add_child(obstacle)
-		obstacle.game_over.connect(_on_game_over)
-		#calculate_max_distance()
-		#up_or_down = randi_range(0, 1)
-#		if up_or_down == 0:
-#			# TODO: generate obstacles so that player can go down
-#			var flying_enemy = flying_enemy_scene.instantiate()
-#			flying_enemy.position.x = screen_size.x
-#			flying_enemy.position.y = randi_range($Ceiling.position.y, min_player_y)
-#			add_child(flying_enemy)
-#		elif up_or_down == 1:
-#			# TODO: generate obstacles so that player can go up
-#			var flying_enemy = flying_enemy_scene.instantiate()
-#			flying_enemy.position.x = screen_size.x
-#			flying_enemy.position.y = randi_range(max_player_y, $Floor.position.y)
-#			add_child(flying_enemy)
-#		up_or_down = -1
+		generate_obstacle()
+	score += roundi(delta * 100)
+
 
 func _physics_process(delta):
 	pass
@@ -67,8 +35,27 @@ func _on_game_over():
 func generate_interval():
 	interval = randf_range(min_interval, max_interval)
 
-#func calculate_max_distance():
-#	max_player_y = $Player.position.y + ($Player.fly_speed * interval * interval) / 2
-#	max_player_y = clamp(max_player_y, $Ceiling.position.y, $Floor.position.y)
-#	min_player_y = $Player.position.y + (ProjectSettings.get_setting("physics/2d/default_gravity") * interval * interval) / 2
-#	min_player_y = clamp(min_player_y, $Ceiling.position.y, $Floor.position.y)
+func generate_obstacle():
+	var obstacle_scale = randf_range(0.5, 2)
+	var random_float = randf()
+	if random_float < 0.6:
+		obstacle = flying_enemy_scene.instantiate()
+		obstacle.position.y = randi_range($Ceiling.position.y, $Floor.position.y - obstacle.get_node("FlyingEnemySprite").get_rect().size.y * obstacle_scale)
+	elif random_float < 0.8:
+		obstacle = spike_down_scene.instantiate()
+		obstacle.position.y = $Ceiling.position.y
+	elif random_float < 0.9:
+		obstacle = spike_up_scene.instantiate()
+		obstacle.position.y = $Floor.position.y - obstacle.get_node("SpikePointsUpSprite").get_rect().size.y * obstacle_scale
+	else:
+		obstacle = standing_enemy_scene.instantiate()
+		obstacle.position.y = $Floor.position.y - obstacle.get_node("StandingEnemySprite").get_rect().size.y * obstacle_scale
+	obstacle.position.x = screen_size.x
+	obstacle.scale.x = obstacle_scale
+	obstacle.scale.y = obstacle_scale
+	add_child(obstacle)
+	obstacle.game_over.connect(_on_game_over)
+
+
+func _on_score_timer_timeout():
+	$HUD/Score.text = "Score: " + str(score)
